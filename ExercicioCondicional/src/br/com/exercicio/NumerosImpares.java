e9fcd9b9a1e807ccf0e235213c0f55cd612c8cd9
Pedro Lucas de Oliveira Bezerra <pedrolucas.nft2021@gmail.com>
2024-10-17 16:30:56 -0300

    Condicional

---
+++ ExercicioCondicional/src/br/com/exercicio/NumerosImpares.java
@@ -0,0 +1,18 @@
+package br.com.exercicio;
+
+import java.util.Scanner;
+
+public class NumerosImpares {
+	
+	public static void Main(String args[]) {
+		int impar;
+		int par;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um número inteiro:");
+		
+		
+	}
+
+}
